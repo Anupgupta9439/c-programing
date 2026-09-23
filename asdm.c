6fcd09e8d7fb4c7df4ebeaaf08f8cc2d84f03ff2
ANUP GUPTA <anupgupta9439@gmail.com>
2026-09-23 19:18:30 +0530

    Add files via upload

---
+++ asdm.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{int a,b;
+    scanf("%d%d",&a,&b);
+    printf("sum of two no is= %d\n",a+b);
+    printf("sub of two no is= %d\n",a-b);
+    printf("mod of two no is= %d\n",a%b);
+    printf("div of two no is= %d\n",a/b);
+    printf("mult of two no is %d\n",a*b);
+    return 0;
+}
